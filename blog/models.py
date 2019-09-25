e0a66901dbcbd581df9d32337b95a0124f9923f2
robstudy <robgarza@mail.fresnostate.edu>
2019-09-25 09:58:31 -0700

    added tags to Blog model and all_tags function. blog_detail.html displays tags split by ','

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import re
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
+    tags = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
  
@@ -9,5 +11,10 @@ class Blog(models.Model):
         return self.body[:100]
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e, %Y')
+    def all_tags(self):
+    	tag_list = re.split(',', self.tags)
+    	for i in range(len(tag_list)):
+    		tag_list[i] = tag_list[i].strip()
+    	return tag_list
     def __str__(self):
         return self.title
