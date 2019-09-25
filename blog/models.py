901e9ad45141d01c000034803c6697ff5198d909
robstudy <robgarza@mail.fresnostate.edu>
2019-09-25 12:38:30 -0700

    new images added for email, tweet, random, book. Blog model functions added - contains_tag, get_image_name

--- blog/models.py
+++ blog/models.py
@@ -16,5 +16,21 @@ class Blog(models.Model):
     	for i in range(len(tag_list)):
     		tag_list[i] = tag_list[i].strip()
     	return tag_list
+    def contains_tag(self, tag_term):
+    	alltags = self.all_tags()
+    	return tag_term in alltags
+    def get_image_name(self):
+    	#reference static/images for list
+    	alltags = self.all_tags()
+    	if 'c++' in alltags:
+    		return 'cpp_logo.png'
+    	elif 'python' in alltags:
+    		return 'python.png'
+    	elif 'javascript' in alltags:
+    		return 'js.png'
+    	elif 'book' in alltags:
+    		return 'book.jpg'
+    	else:
+    		return 'random.png'
     def __str__(self):
         return self.title
