55779f9f4a2db652a91a0c5ac6c1d999579a7e23
robstudy <robgarza@mail.fresnostate.edu>
2019-09-29 10:53:53 -0700

    projects page updated to show cards with project image, name, and updated date

--- projects/views.py
+++ projects/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 def projects(request):
-	return render(request, 'projects/projects.html')
+	projects = Project.objects.order_by('-pub_date')
+	return render(request, 'projects/projects.html', {'projects': projects})
 
 def project_detail(request, project_name):
 	name = project_name.replace('-', ' ')
 	project = get_object_or_404(Project, title=name)
 	return render(request, 'projects/project_detail.html', {'project': project})
-
-
