9ca7dfc8065c07c49eab1beb6f558a25365e73ed
robstudy <robgarza@mail.fresnostate.edu>
2019-10-23 15:30:12 -0700

    changed debug to false

--- TheFifthMoon/settings.py
+++ TheFifthMoon/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'thefifthmoon.xyz', 'www.thefifthmoon.xyz', 'https://thefifthmoon.herokuapp.com/']
 
